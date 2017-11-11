b39ca1a30244dfb3682bd6c376fab049929b0fd7
AM-Kelly <33530957+AM-Kelly@users.noreply.github.com>
2017-11-11 18:44:03 +0000

    Simple Text Change

--- app/src/main/java/com/example/adam/bsp/MainActivity.java
+++ app/src/main/java/com/example/adam/bsp/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "This button will allow for searching through books", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
